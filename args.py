0d5309852703d0d56d444857230a0446a31c5bc9
Andrei Odintsov <forestryks1@gmail.com>
2023-01-31 11:49:52 +0000

    Add readme

--- args.py
+++ args.py
@@ -9,6 +9,7 @@ class Args:
     address: str
     port: int
     hyperlinks_enabled: bool
+    profiler_url: str
 
 
 def parse_args() -> Args:
@@ -16,7 +17,8 @@ def parse_args() -> Args:
     parser.add_argument("--address", help="set address to serve on, defaults to 0.0.0.0", default="0.0.0.0", type=str)
     parser.add_argument("--port", help="set port to serve on, defaults to random", default=0, type=int)
     parser.add_argument("--no-hyperlinks", help="disable hyperlinks", action="store_true")
-    parser.add_argument("file", help="path to file to serve, defaults to perf.txt", nargs="?", default=Path("perf.data"), type=Path)
+    parser.add_argument("--profiler-url", help="change profiler url used for generating links", default="profiler.forestryks.org", type=str)
+    parser.add_argument("file", help="path to perf.data, defaults to perf.data in current directory", nargs="?", default=Path("perf.data"), type=Path)
     args = parser.parse_args()
 
-    return Args(args.file, args.address, args.port, not args.no_hyperlinks)
+    return Args(args.file, args.address, args.port, not args.no_hyperlinks, args.profiler_url)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from server import start_server
 from time import sleep
 from perf_script import run_perf_script
-from args import parse_args
+from args import Args, parse_args
 from interfaces import get_local_ips
 from typing import List, Tuple, Optional
 import urllib.parse
@@ -24,10 +24,10 @@ def get_my_ip() -> str:
     return requests.get('https://api.ipify.org').text
 
 
-def display_url_via_ip(ip: str, port: int, prefix: str, name: str, hyperlinks_enabled: bool):
+def display_url_via_ip(ip: str, port: int, prefix: str, name: str, args: Args):
     data_url = f"http://{ip}:{port}/"
-    url = f"https://profiler.forestryks.org/from-url/{urllib.parse.quote(data_url, safe='')}"
-    if hyperlinks_enabled:
+    url = f"https://{args.profiler_url}/from-url/{urllib.parse.quote(data_url, safe='')}"
+    if args.hyperlinks_enabled:
         link = make_hyperlink(name, url)
         print(f"{prefix} {link}")
     else:
@@ -48,29 +48,29 @@ def remove_ip_from_list(ips: List[Tuple[str, str]], to_remove: str):
     ips.pop(index)
 
 
-def try_display_public(ips: List[Tuple[str, str]], port: int, hyperlinks_enabled: bool):
+def try_display_public(ips: List[Tuple[str, str]], port: int, args: Args):
     public_ip = get_my_ip()
     if public_ip is not None:
         remove_ip_from_list(ips, public_ip)
-        display_url_via_ip(public_ip, port, "🚀", f"via public ip ({public_ip})", hyperlinks_enabled)
+        display_url_via_ip(public_ip, port, "🚀", f"via public ip ({public_ip})", args)
 
 
-def try_display_vpn(ips: List[Tuple[str, str]], port: int, hyperlinks_enabled: bool):
+def try_display_vpn(ips: List[Tuple[str, str]], port: int, args: Args):
     vpn_ip = find_vpn_ip(ips)
     if vpn_ip is not None:
         remove_ip_from_list(ips, vpn_ip)
-        display_url_via_ip(vpn_ip, port, "🚀", f"via VPN ({vpn_ip})", hyperlinks_enabled)
+        display_url_via_ip(vpn_ip, port, "🚀", f"via VPN ({vpn_ip})", args)
 
 
-def display_possible_urls(port: int, hyperlinks_enabled: bool):
+def display_possible_urls(port: int, args: Args):
     ips = get_local_ips()
-    try_display_public(ips, port, hyperlinks_enabled)
-    try_display_vpn(ips, port, hyperlinks_enabled)
+    try_display_public(ips, port, args)
+    try_display_vpn(ips, port, args)
 
     if len(ips) > 0:
         print("Other variants:")
         for ip in ips:
-            display_url_via_ip(ip[1], port, "➤", f"via {ip[1]}", hyperlinks_enabled)
+            display_url_via_ip(ip[1], port, "➤", f"via {ip[1]}", args)
 
 
 def display_hyperlinks_warning():
@@ -89,7 +89,7 @@ def main():
     if args.hyperlinks_enabled:
         display_hyperlinks_warning()
     print()
-    display_possible_urls(http_server.server_address[1], args.hyperlinks_enabled)
+    display_possible_urls(http_server.server_address[1], args)
 
     while True:
         sleep(1)
